bf788d87fd70f403fb61e94e7bcb8aeaf05535f3
Saulius Žemaitaitis <saulius@zemaitaitis.lt>
2015-01-24 21:27:20 +0200

    Add player ready RPC dance.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
+	private PhotonView photonView;
+	private GameObject[] spawnPoints;
+	private HashSet<PhotonPlayer> readyPlayers;
 	enum GameState
 	{
 		STARTING,
 		RUNNING,
 		FINISHED
-
 	}
 
 	public int[] score;
@@ -19,11 +22,48 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		photonView = GetComponent<PhotonView>();
+		GameObject[] SpawnPoints = GameObject.FindGameObjectsWithTag("Respawn");
+		PrepareNewGame();
+	}
 
+	void PrepareNewGame() {
+		readyPlayers = new HashSet<PhotonPlayer>();
+	}
+
+	void StartMatch() {
+		int i = 0;
+		foreach(PhotonPlayer player in PhotonNetwork.playerList) {
+			photonView.RPC("ClientStartMatch", player, i);
+			i += 1;
+		}
+	}
+
+	public void PlayerReady() {
+		photonView.RPC("RPCPlayerReady", PhotonTargets.MasterClient, PhotonNetwork.player);
+	}
+
+	[RPC]
+	void RPCPlayerReady (PhotonPlayer player) {
+		if(!PhotonNetwork.isMasterClient) {
+			Debug.Log("We're not master but received ready");
+			return;
+		}
+
+		readyPlayers.Add(player);
+
+		if (readyPlayers.Count == PhotonNetwork.playerList.Length) {
+			StartMatch();
+		}
+	}
+
+	[RPC]
+	void ClientStartMatch (int spawnPointNum) {
+		Debug.Log("Starting in position " + spawnPointNum);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
