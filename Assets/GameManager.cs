5711ec093da01f01a05f38abba3780d6ef9ad434
Ignas Mikalajunas <ignas@uber.com>
2015-01-24 19:34:19 +0200

    Tweaked speeds, added some UI

---
+++ Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	enum GameState
+	{
+		STARTING,
+		RUNNING,
+		FINISHED
+
+	}
+
+	public int[] score;
+	public int players = 2;
+	public Transform level;
+	public Transform player;
+	private GameState currentState = GameState.STARTING;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
